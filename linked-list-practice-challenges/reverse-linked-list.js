986b631db25b3aefcf2ed25de9b4d95073aac731
Seremba <pserembae@gmail.com>
2022-08-04 09:43:41 +0300

    solve reverse-linked-list challenge

--- linked-list-practice-challenges/reverse-linked-list.js
+++ linked-list-practice-challenges/reverse-linked-list.js
@@ -4,3 +4,41 @@ list as an argument. The function should reverse the order of the
 nodes in the linked
 list in-place and return the new head of the reversed linked list
 */
+
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+let reverseList = (head) => {
+    let prev = null;
+    let current = head;
+
+    while(current !== null){
+        const next = current.next;
+        current.next = prev;
+        prev = current
+        current = next;
+    }
+
+    return prev;
+}
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+e.next = f;
+
+// a -> b -> c -> d -> e -> f
+
+console.log(reverseList(a)); // f -> e -> d -> c -> b -> a
