09e5c8a515e9bb2fbe8c166850342aa0aef0c50e
Seremba <pserembae@gmail.com>
2022-08-04 15:31:11 +0300

    solve zipper challenge recursively

--- linked-list-practice-challenges/zipper-recursive.js
+++ linked-list-practice-challenges/zipper-recursive.js
@@ -15,34 +15,22 @@ class Node {
       this.val = val;
       this.next = null;
     }
-  }
+}
+ const zipperLists = (head1, head2) => {
+    if(head1 === null && head2 === null) return null;
+    if(head1 === null) return head2;
+    if(head2 === null) return head1;
 
-
-const zipperLists = (head1, head2) => {
-    let tail = head1;
-    let current1 = head1.next;
-    let current2 = head2;
-    let count = 0;
-
-    while (current1 !== null && current2 !== null){
-        if(count % 2 === 0){
-            tail.next = current2;
-            current2 = current2.next;
-        }else {
-            tail.next = current1;
-            current1 = current1.next;
-        }
-        tail = tail.next;
-        count += 1;
-    }
-
-    if(current1 !== null) tail.next = current1;
-    if(current2 !== null) tail.next = current2;
+    const next1 = head1.next;
+    const next2 = head2.next;
+    head1.next = head2;
+    head2.next = zipperLists(next1, next2);
 
     return head1;
-}
+ }
 
-const a = new Node("a");
+
+ const a = new Node("a");
 const b = new Node("b");
 const c = new Node("c");
 a.next = b;
@@ -56,5 +44,5 @@ x.next = y;
 y.next = z;
 // x -> y -> z
 
-zipperLists(a, x);
+console.log(zipperLists(a, x));
 // a -> x -> b -> y -> c -> z
