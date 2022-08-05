34dca588be51aba7cb18e09d329553683742b59f
Seremba <pserembae@gmail.com>
2022-08-05 17:35:36 +0300

    solve max-path-sum and tree-min-value challenges

---
+++ binary-tree-chllenges/tree-min-value-recursive.js
@@ -0,0 +1,43 @@
+/*
+Write a function, treeMinValue, that takes in the root of a binary
+tree that contains number values. The function should return the
+minimum value within the tree.
+
+You may assume that the input tree is non-empty.
+*/
+class Node {
+    constructor(val){
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const treeMinValue = (root) => {
+    if(root === null) return Infinity;
+
+    const leftValues = treeMinValue(root.left);
+    const rightValues = treeMinValue(root.right);
+    return Math.min(root.val, leftValues, rightValues);
+}
+
+const a = new Node(3);
+const b = new Node(11);
+const c = new Node(4);
+const d = new Node(4);
+const e = new Node(-2);
+const f = new Node(1);
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//       3
+//    /    \
+//   11     4
+//  / \      \
+// 4   -2     1
+
+console.log(treeMinValue(a)); // -> -2
