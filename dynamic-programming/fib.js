94636d51010956eb12546409e572cddcc5b30213
Seremba <pserembae@gmail.com>
2022-08-18 00:18:39 +0300

    solve fibanocci challenge using dynamic programming

---
+++ dynamic-programming/fib.js
@@ -0,0 +1,16 @@
+/*write a function fib(n) that takes in a number as an argument. The
+function should return the n-th number of the fibonacci sequence */
+
+// use memoization, use js object. keys is the fn argument, values are the return value
+const fib = (n, memo = {}) => {
+    if(n in memo) return memo[n];
+    if(n <= 2) return 1;
+
+    memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+
+    return memo[n];
+}
+
+console.log(fib(7))
+console.log(fib(5))
+console.log(fib(50))
