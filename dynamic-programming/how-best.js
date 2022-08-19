c78eabecc04c2663a342338adab910013b27a707
Seremba <pserembae@gmail.com>
2022-08-19 05:34:00 +0300

    Improve the efficiency of how-best challenge

--- dynamic-programming/how-best.js
+++ dynamic-programming/how-best.js
@@ -8,14 +8,15 @@ up to exactly the targetSum.
 If there is a tie for the shortest combination, you may return anyone of the shortest
 */
 
-const bestSum = (targetSum, numbers) => {
+const bestSum = (targetSum, numbers, memo = {}) => {
+    if(targetSum in memo) return memo[targetSum];
     if(targetSum === 0) return [];
     if(targetSum < 0) return null;
 
     let shortestCombination = null;
     for(let num of numbers) {
         const remainder = targetSum - num;
-        const remainderCombination = bestSum(remainder, numbers);
+        const remainderCombination = bestSum(remainder, numbers, memo);
 
         if(remainderCombination !== null) {
           let combination = [...remainderCombination, num];
@@ -24,10 +25,11 @@ const bestSum = (targetSum, numbers) => {
           }
         }
     }
-    return shortestCombination;
+    memo[targetSum] = shortestCombination;
+    return memo[targetSum];
 }
 
 console.log(bestSum(7, [5, 3, 4, 7])); // [3, 4]
 console.log(bestSum(8, [2, 3, 5])); // [2, 2, 2, 2]
 console.log(bestSum(8, [ 1, 4, 5])); // [4, 4]
-console.log(bestSum(100, [1, 2, 5, 25]));
+console.log(bestSum(100, [1, 2, 5, 25])); // [25, 25, 25, 25]
