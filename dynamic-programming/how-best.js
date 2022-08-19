2205bc6d9507779ce9e9ba036a1ba1ab76817ffb
Seremba <pserembae@gmail.com>
2022-08-19 05:29:21 +0300

    solve how-best dynamic programming challenge

---
+++ dynamic-programming/how-best.js
@@ -0,0 +1,33 @@
+/*
+write a function bestSum(targetSum, numbers) that takes in target sum and an array of numbers
+as arguments.
+
+The function should return an array containing the shortest combination of numbers that add
+up to exactly the targetSum.
+
+If there is a tie for the shortest combination, you may return anyone of the shortest
+*/
+
+const bestSum = (targetSum, numbers) => {
+    if(targetSum === 0) return [];
+    if(targetSum < 0) return null;
+
+    let shortestCombination = null;
+    for(let num of numbers) {
+        const remainder = targetSum - num;
+        const remainderCombination = bestSum(remainder, numbers);
+
+        if(remainderCombination !== null) {
+          let combination = [...remainderCombination, num];
+          if(shortestCombination === null || combination.length < shortestCombination.length) {
+              shortestCombination = combination;
+          }
+        }
+    }
+    return shortestCombination;
+}
+
+console.log(bestSum(7, [5, 3, 4, 7])); // [3, 4]
+console.log(bestSum(8, [2, 3, 5])); // [2, 2, 2, 2]
+console.log(bestSum(8, [ 1, 4, 5])); // [4, 4]
+console.log(bestSum(100, [1, 2, 5, 25]));
