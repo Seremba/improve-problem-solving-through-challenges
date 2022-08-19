21f1b1470a4e1532c6b33406ea34a4ac41b11bf5
Seremba <pserembae@gmail.com>
2022-08-19 06:24:19 +0300

    solve can-construct dynamic programming challenge

---
+++ dynamic-programming/can-construct.js
@@ -0,0 +1,38 @@
+/*
+write a function canConstruct(target, wordBank) that accepts a target string and an array
+of strings.
+
+The function should return a boolean, indicating whether or not the target can be constructed
+by concatenating elements of the wordBank array.
+
+You may reuse elements of the wordBank as many times as needed.
+*/
+
+const canConstruct = (target, wordBank) => {
+    if(target === '') {
+        return true;
+    }
+
+    for(let word of wordBank){
+        if (target.indexOf(word) === 0) {
+            const suffix = target.slice(word.length);
+            if(canConstruct(suffix, wordBank) === true){
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+
+console.log(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])) // true
+console.log(canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])) // false
+console.log(canConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])) // true
+console.log(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
+ ["e",
+ "eee",
+ "eeee",
+ "eeeee",
+ "eeeeee",
+ "eeeeeee"])) // false
