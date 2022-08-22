c27f3b9d09038354df3de55e5b734c7e739a2fc3
Seremba <pserembae@gmail.com>
2022-08-22 05:25:02 +0300

    solve count-construct dynamic challenge

---
+++ dynamic-programming/count-construct.js
@@ -0,0 +1,36 @@
+/*
+write a function countConstruct(target, wordBank) that accepts a target string and an array
+of strings.
+
+The function should return the number of ways that the `target` can be constructed by
+concatinating elements of the `wordBank` array.
+
+You may reuse elements of the wordBank as many times as needed.
+*/
+
+const countConstruct = (target, wordBank) => {
+    if(target === '') return 1;
+
+    let count = 0;
+    for(let word of wordBank){
+        if(target.indexOf(word) === 0) {
+            const suffix = target.slice(word.length);
+            const waysOfRest = countConstruct(suffix, wordBank);
+            count += waysOfRest;
+        }
+    }
+
+    return count;
+}
+
+
+console.log(countConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])) // 1
+console.log(countConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])) // 0
+console.log(countConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])) // 4
+console.log(countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
+ ["e",
+ "eee",
+ "eeee",
+ "eeeee",
+ "eeeeee",
+ "eeeeeee"])) // 0
