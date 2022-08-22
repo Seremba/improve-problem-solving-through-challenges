d8d97bc988fb8de610c04e58ae81d04f6dab9ef1
Seremba <pserembae@gmail.com>
2022-08-22 05:28:27 +0300

    improve efficiency of count-construct dyanamic programming challenge

--- dynamic-programming/count-construct.js
+++ dynamic-programming/count-construct.js
@@ -8,19 +8,21 @@ concatinating elements of the `wordBank` array.
 You may reuse elements of the wordBank as many times as needed.
 */
 
-const countConstruct = (target, wordBank) => {
+const countConstruct = (target, wordBank, memo = {}) => {
+    if(target in memo) return memo[target];
     if(target === '') return 1;
 
     let count = 0;
     for(let word of wordBank){
         if(target.indexOf(word) === 0) {
             const suffix = target.slice(word.length);
-            const waysOfRest = countConstruct(suffix, wordBank);
+            const waysOfRest = countConstruct(suffix, wordBank, memo);
             count += waysOfRest;
         }
     }
 
-    return count;
+    memo[target] = count;
+    return memo[target];
 }
 
 
