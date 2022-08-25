9bf85da68375d79d04718288eb0dd49514936c84
Seremba <pserembae@gmail.com>
2022-08-25 06:06:41 +0300

    solve can-construst challenge using DP tabular technique

---
+++ dynamic-programming/tabulation/count-construct.js
@@ -0,0 +1,35 @@
+/*
+write a function countConstruct(target, wordBank) that accepts a target string and an array
+of strings.
+
+The function should return the number of ways that the `target` can be constructed by
+concatenating elements of the `wordBank` array.
+
+You may reuse elements of the wordBank as many times as needed.
+*/
+
+const countConstruct = (target, wordBank) => {
+    const table = Array(target.length + 1).fill(0);
+    table[0] = 1;
+
+    for(let i = 0; i < target.length; i++){
+        for(let word of wordBank) {
+            if(target.slice(i, i + word.length) === word){
+                table[i + word.length] += table[i];
+            }
+        }
+    }
+
+    return table[target.length];
+}
+
+console.log(countConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])) // 1
+console.log(countConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])) // 0
+console.log(countConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])) // 4
+console.log(countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
+ ["e",
+ "eee",
+ "eeee",
+ "eeeee",
+ "eeeeee",
+ "eeeeeee"])) // 0
