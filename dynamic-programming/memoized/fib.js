0d692a2377544732c3eefb59e39f47ddac19d4ab
Seremba <pserembae@gmail.com>
2022-09-28 10:26:16 +0300

    improve fib DP challenge

--- dynamic-programming/memoized/fib.js
+++ dynamic-programming/memoized/fib.js
@@ -3,14 +3,16 @@ function should return the n-th number of the fibonacci sequence */
 
 // use memoization, use js object. keys is the fn argument, values are the return value
 const fib = (n, memo = {}) => {
-    if(n in memo) return memo[n];
-    if(n <= 2) return 1;
+  // if the arg is already in the memo, then fetch the value
+  if (n in memo) return memo[n];
+  if (n <= 2) return 1;
 
-    memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  // if the arg is not in the memo, calculate it recursively but save it in the memo for future
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
 
-    return memo[n];
-}
+  return memo[n];
+};
 
-console.log(fib(7))
-console.log(fib(5))
-console.log(fib(50))
+console.log(fib(7));
+console.log(fib(5));
+console.log(fib(50));
