6e5821ff8262bbd696aef18324a7c3058af567a1
Seremba <pserembae@gmail.com>
2022-10-03 23:45:55 +0300

    get better at insertion sort

--- sorting/insertion_sort/insertion_sort.js
+++ sorting/insertion_sort/insertion_sort.js
@@ -8,7 +8,7 @@ function insertionSort(arr) {
     let currElement = arr[i];
 
     // the `j` loop will iterate left through the sorted region,
-    // looking for a legal spot to insert currElement
+    // looking for a legal spot to insert currElement/////-
     for (var j = i - 1; j >= 0 && currElement < arr[j]; j--) {
       // keep moving left while currElement is less than the j-th element
 
