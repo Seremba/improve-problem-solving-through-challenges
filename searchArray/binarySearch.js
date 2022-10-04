2a23832d1bc1f88a38ddf52cbf9f047c24c8125e
Seremba <pserembae@gmail.com>
2022-10-04 23:32:45 +0300

    improve binary search algorthm method

--- searchArray/binarySearch.js
+++ searchArray/binarySearch.js
@@ -17,18 +17,15 @@ function binarySearch(array, target) {
 // console.log(binarySearch([5, 10, 12, 15, 20, 30, 70], 12));
 // console.log(binarySearch([1, 2, 2, 3], 1));
 
-function binarySearchIndex(array, target) {
-  if (!array.length) return -1;
+function binarySearchIndex(array, target, low = 0, high = array.length - 1) {
+  if (low === high) return -1;
 
-  let midIndex = Math.floor(array.length / 2);
-
-  let leftArray = array.slice(0, midIndex);
-  let rightArray = array.slice(midIndex + 1);
+  let midIndex = Math.floor((low + high) / 2);
 
   if (target < array[midIndex]) {
-    return binarySearchIndex(leftArray, target);
+    return binarySearchIndex(array, target, low, midIndex);
   } else if (target > array[midIndex]) {
-    return binarySearchIndex(rightArray, target);
+    return binarySearchIndex(array, target, midIndex + 1, high);
   } else {
     return midIndex;
   }
